2812840c2820963b18876e218f907478c0e92607
1436120982@qq.com <13179720640yshx>
2019-08-15 17:12:56 +0800

    fafasfasf

---
+++ ResidenceModel.m
@@ -0,0 +1,14 @@
+//
+//  ResidenceModel.m
+//  fmdb_pickviewController
+//
+//  Created by lcy on 16/4/7.
+//  Copyright © 2016年 ZG. All rights reserved.
+//
+
+#import "ResidenceModel.h"
+
+@implementation ResidenceModel
+
+
+@end
